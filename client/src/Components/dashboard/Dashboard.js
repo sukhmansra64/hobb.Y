e65a42e9dc074ce8d3825c4073f52c02a1eaf038
sukhmansra64 <ssra5@uwo.ca>
2021-09-13 13:18:57 -0400

    Update
    Adjusted UI to allow for project.

--- client/src/Components/dashboard/Dashboard.js
+++ client/src/Components/dashboard/Dashboard.js
@@ -4,92 +4,24 @@ import {Redirect} from 'react-router-dom'
 import PropTypes from 'prop-types';
 
 Dashboard.propTypes = {
+    user: PropTypes.object,
     isAuthenticated: PropTypes.bool.isRequired
 };
 
-function Dashboard({isAuthenticated}) {
+function Dashboard({isAuthenticated, user}) {
     if(!isAuthenticated) return(<Redirect to='/login'/>)
     return (
         <section className="container">
             <h1 className="large text-primary">
                 Dashboard
             </h1>
-            <p className="lead"><i className="fas fa-user"></i> Welcome John Doe</p>
+            <p className="lead"><i className="fas fa-user"></i> Welcome </p>
             <div className="dash-buttons">
                 <a href="create-profile.html" className="btn btn-light"
                 ><i className="fas fa-user-circle text-primary"></i> Edit Profile</a
                 >
-                <a href="add-experience.html" className="btn btn-light"
-                ><i className="fab fa-black-tie text-primary"></i> Add Experience</a
-                >
-                <a href="add-education.html" className="btn btn-light"
-                ><i className="fas fa-graduation-cap text-primary"></i> Add Education</a
-                >
+
             </div>
-
-            <h2 className="my-2">Experience Credentials</h2>
-            <table className="table">
-                <thead>
-                <tr>
-                    <th>Company</th>
-                    <th className="hide-sm">Title</th>
-                    <th className="hide-sm">Years</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Tech Guy Web Solutions</td>
-                    <td className="hide-sm">Senior Developer</td>
-                    <td className="hide-sm">
-                        02-03-2009 - 01-02-2014
-                    </td>
-                    <td>
-                        <button className="btn btn-danger">
-                            Delete
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Sukhman Sra</td>
-                    <td className="hide-sm">Hobbyist Programmer & hobbyist Pianist</td>
-                    <td className="hide-sm">
-                        02-03-2015 - Now
-                    </td>
-                    <td>
-                        <button className="btn btn-danger">
-                            Delete
-                        </button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-
-            <h2 className="my-2">Education Credentials</h2>
-            <table className="table">
-                <thead>
-                <tr>
-                    <th>School</th>
-                    <th className="hide-sm">Degree</th>
-                    <th className="hide-sm">Years</th>
-                    <th/>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Northern Essex</td>
-                    <td className="hide-sm">Associates</td>
-                    <td className="hide-sm">
-                        02-03-2007 - 01-02-2009
-                    </td>
-                    <td>
-                        <button className="btn btn-danger">
-                            Delete
-                        </button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
 
             <div className="my-2">
                 <button className="btn btn-danger">
@@ -103,6 +35,7 @@ function Dashboard({isAuthenticated}) {
 }
 
 const mapStateToProps = (state) => ({
+    user: state.auth.user,
     isAuthenticated: state.auth.isAuthenticated
 });
 
