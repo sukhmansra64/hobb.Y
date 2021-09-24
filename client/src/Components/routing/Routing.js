7fb03134c66b5ffb6290a4f3aad96cdc54ac9764
SukhmanSra <ssra5@uwo.ca>
2021-09-24 12:04:41 -0400

    Update
    Added a dedicated routing component.

---
+++ client/src/Components/routing/Routing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+Routing.propTypes = {
+
+};
+
+function Routing(props) {
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default connect(mapStateToProps)(Routing);
