bf0bcf034df55f03f49a9d301d559f25cfb5f32d
SukhmanSra <ssra5@uwo.ca>
2022-01-08 00:10:15 -0500

    make it better

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser')
+const path = require('path')
 const cors = require('cors');
 
 const app = express();
-
-const PORT = process.env.PORT||8000
 
 //initialize middlewares
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
 const auth = require('./middleware/auth');
 
 //routes
@@ -21,11 +21,17 @@ app.use('/api/posts',require('./routes/api/posts'));
 app.use('/api/profile',require('./routes/api/profile'));
 
 
-app.get('/',(req,res)=>{
-    res.send('api running');
-});
-
 connectDB();
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client,build,index.html'))
+    });
+}
+
+const PORT = process.env.PORT||8000
 
 app.listen(PORT,()=>{
     console.log(`Server listening on port ${PORT}`)
