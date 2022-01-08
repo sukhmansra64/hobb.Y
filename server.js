ba7de25226470bba603b097ad54f7ee5858b7794
SukhmanSra <ssra5@uwo.ca>
2022-01-08 02:06:44 -0500

    Update

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ app.use('/api/profile',require('./routes/api/profile'));
 
 connectDB();
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
 
-    app.get('*',(req,res)=>{
+app.use(express.static('client/build'));
+
+app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client,build,index.html'))
-    });
-}
+});
+
 
 const PORT = process.env.PORT||8000
 
