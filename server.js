1eede696e7fafc137fb75eaf5fcef09c4efb758d
SukhmanSra <ssra5@uwo.ca>
2023-01-25 20:41:05 -0500

    Hotfix: ec2 adjustment

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ connectDB();
 app.use(express.static('client/build'));
 
 app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client,build,index.html'))
+        res.sendFile(path.resolve(__dirname,'client/build'))
 });
 
 const PORT = process.env.PORT||8080
